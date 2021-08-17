eca6c7cf96393007dd9d7f2d81d205a675f0d791
Simone Zini <simonezini1@gmail.com>
2021-08-17 15:20:19 +0200

    Add docker container creation
    Added scripts to quickly create a docker container from source code

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM scratch
+COPY ./planning .
+COPY ./bapomdp .
+COPY ./fbapomdp .
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo Building simonezini97/fba-pomdp:build
+
+docker build -t simonezini97/fba-pomdp:build . -f Dockerfile.build
+
+docker container create --name extract simonezini97/fba-pomdp:build  
+docker container cp extract:/fba-pomdp/planning ./
+docker container cp extract:/fba-pomdp/bapomdp ./
+docker container cp extract:/fba-pomdp/fbapomdp ./
+docker container rm -f extract
+
+echo Building simonezini97/fba-pomdp:latest
+
+docker build --no-cache -t simonezini97/fba-pomdp:latest .
+rm ./planning
+rm ./bapomdp
+rm ./fbapomdp
